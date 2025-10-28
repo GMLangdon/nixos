b4e623f57a429bb2720f22ca957a0239414a5f85
George Langdon <correct.threaten770@4wrd.cc>
2025-10-28 09:59:26 +0000

    Batman

---
+++ configuration.nix
@@ -0,0 +1,280 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.supportedFilesystems = [ "nfs" ];
+  # Use latest kernel.
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+  
+
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+    settings = {
+      General = {
+        # Shows battery charge of connected devices on supported
+        # Bluetooth adapters. Defaults to 'false'.
+        Experimental = true;
+        # When enabled other devices can connect faster to us, however
+        # the tradeoff is increased power consumption. Defaults to
+        # 'false'.
+        FastConnectable = true;
+      };
+      Policy = {
+        # Enable all controllers when they are found. This includes
+        # adapters present on start as well as adapters that are plugged
+        # in later on. Defaults to 'true'.
+        AutoEnable = true;
+      };
+    };
+  };
+
+  services.blueman.enable = true;
+
+  services.libinput = {
+    enable = true;
+    mouse = {
+      accelProfile = "adaptive";  # or "flat"
+      accelSpeed = "1.0";         # must be quoted
+    };
+  };
+
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/London";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_GB.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_GB.UTF-8";
+    LC_IDENTIFICATION = "en_GB.UTF-8";
+    LC_MEASUREMENT = "en_GB.UTF-8";
+    LC_MONETARY = "en_GB.UTF-8";
+    LC_NAME = "en_GB.UTF-8";
+    LC_NUMERIC = "en_GB.UTF-8";
+    LC_PAPER = "en_GB.UTF-8";
+    LC_TELEPHONE = "en_GB.UTF-8";
+    LC_TIME = "en_GB.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  # You can disable this if you're only using the Wayland session.
+  services.xserver.enable = true;
+
+  # Enable the KDE Plasma Desktop Environment.
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "gb";
+    variant = "extd";
+  };
+
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
+
+
+  # Configure console keymap
+  console.keyMap = "uk";
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.gandalf = {
+    isNormalUser = true;
+    description = "gandalf";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+      kdePackages.kate
+      keepassxc 
+    #  thunderbird
+    ];
+  };
+
+  services.openssh.enable = true;
+  programs.ssh.startAgent = true;
+
+  # Install firefox.
+  programs.firefox.enable = true;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  services.mullvad-vpn.enable = true;
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    vim
+    wget
+    git
+    openssh
+    libnotify
+    nodejs_22
+    yarn
+    python3
+    pkg-config
+    gcc
+    gnumake
+    nextcloud-client
+    vscode
+    mullvad
+    mullvad-vpn
+    nfs-utils
+    transmission-gtk
+    slack
+    teams-for-linux
+    steam
+    gitkraken
+    heroic
+
+
+    # Salesforce CLI via flake
+    (builtins.getFlake "github:rfaulhaber/sfdx-nix").packages.${pkgs.system}.default
+  ];
+
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;   # optional, for streaming from/to this box
+    dedicatedServer.openFirewall = true; # optional, if you host anything
+  };
+
+
+  fileSystems."/mnt/tv" = {
+    device = "192.168.68.76:/mnt/tank/media/tv";
+    fsType = "nfs";
+      options = [
+        "nfsvers=4"
+        "proto=tcp"
+        "_netdev"
+        "noatime"
+        "x-systemd.automount"
+        "noauto"
+      ];
+  };
+
+  fileSystems."/mnt/movies" = {
+    device = "192.168.68.76:/mnt/tank/media/movies";
+    fsType = "nfs";
+      options = [
+        "nfsvers=4"
+        "proto=tcp"
+        "_netdev"
+        "noatime"
+        "x-systemd.automount"
+        "noauto"
+      ];
+  };
+
+  fileSystems."/mnt/music" = {
+    device = "192.168.68.76:/mnt/tank/media/music";
+    fsType = "nfs";
+      options = [
+        "nfsvers=4"
+        "proto=tcp"
+        "_netdev"
+        "noatime"
+        "x-systemd.automount"
+        "noauto"
+      ];
+  };
+
+  fileSystems."/mnt/tank" = {
+    device = "192.168.68.76:/mnt/tank";
+    fsType = "nfs";
+      options = [
+        "nfsvers=4"
+        "proto=tcp"
+        "_netdev"
+        "noatime"
+        "x-systemd.automount"
+        "noauto"
+      ];
+  };
+
+  fileSystems."/mnt/books" = {
+    device = "192.168.68.76:/mnt/tank/media/books";
+    fsType = "nfs";
+      options = [
+        "nfsvers=4"
+        "proto=tcp"
+        "_netdev"
+        "noatime"
+        "x-systemd.automount"
+        "noauto"
+      ];
+  };
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "25.05"; # Did you read the comment?
+
+}
